be89bd4ceae87e053f2aa5915b2f3fda019c97fb
Eric Hwang <eric_hwang@college.harvard.edu>
2024-09-17 12:41:05 -0400

    fixed typo in resume path

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -6,8 +6,8 @@ export default function ResumeButton() {
     <Button className="flex min-w-14 flex-row px-2 text-center sm:min-w-24">
       <a
         className="flex w-full flex-row items-center justify-center gap-1 text-center"
-        href="/Eric_Hwang_Resume.pdf"
-        download="Eric_Hwang_Resume.pdf"
+        href="/EricHwang_Resume.pdf"
+        download="EricHwang_Resume.pdf"
       >
         <ArrowDownToLine
           size={16}
@@ -19,3 +19,4 @@ export default function ResumeButton() {
     </Button>
   );
 }
+
